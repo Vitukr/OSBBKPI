24e65fead8b3a006026d52af9791d8af3379bddd
Vitalii Yarema <vyarema@windowslive.com>
2019-02-03 14:23:40 +0200

    Add project files.

---
+++ OSBBKPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OSBBKPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
